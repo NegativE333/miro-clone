0c16d22ed5ea3ca5d72ba66d40e82c40799c1a85
Om Tekade <omtekade914@gmail.com>
2024-02-15 12:08:37 +0530

    pencil function add

---
+++ app/board/[boardId]/_components/text.tsx
@@ -0,0 +1,68 @@
+import { cn, colorToCss } from "@/lib/utils";
+import { useMutation } from "@/liveblocks.config";
+import { Textlayer } from "@/types/canvas";
+import { Kalam } from "next/font/google";
+import ContenetEditable, { ContentEditableEvent } from "react-contenteditable";
+
+const font = Kalam({ subsets: ["latin"], weight: ["400"] });
+
+const calculateFontSize = (widht: number, height: number) => {
+  const maxFontSize = 96;
+  const scaleFactor = 0.5;
+  const fontSizeBasedOnHeight = height * scaleFactor;
+  const fontSizeBasedOnWidht = widht * scaleFactor;
+
+  return Math.min(fontSizeBasedOnHeight, fontSizeBasedOnWidht, maxFontSize);
+};
+
+interface TextProps {
+  id: string;
+  layer: Textlayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Text = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: TextProps) => {
+  const { x, y, width, height, fill, value } = layer;
+
+  const updateValue = useMutation(({ storage }, newValue: string) => {
+    const liveLayers = storage.get("layers");
+
+    liveLayers.get(id)?.set("value", newValue);
+  }, []);
+
+  const handleContentChange = (e: ContentEditableEvent) => {
+    updateValue(e.target.value);
+  }
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+      }}
+    >
+      <ContenetEditable
+        html={value || "Text"}
+        onChange={handleContentChange}
+        className={cn(
+          "h-full w-full flex items-center justify-center text-center drop-shadow-md outline-none",
+          font.className
+        )}
+        style={{
+          fontSize: calculateFontSize(width, height),
+          color: fill ? colorToCss(fill) : "#000",
+        }}
+      />
+    </foreignObject>
+  );
+};
